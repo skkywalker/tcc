3aa22bd76d2663d645f2d00658ebabc9c7cf2ad6
skkywalker <lucas@hehl.com.br>
2019-08-06 20:48:17 -0300

    pf: breadth: Made it impossible to go through "0"

--- path-finding/learning_breadth-first-search.py
+++ path-finding/learning_breadth-first-search.py
@@ -11,14 +11,16 @@ class Map:
     def get_neighbors(self,position):
         neighbors = list()
         for i,j in ([0,1],[-1,0],[0,-1],[1,0]):
-            if ((position[0]+i >= 0 and position[0]+i < self.graph.shape[0]) and 
-                (position[1]+j >= 0 and position[1]+j < self.graph.shape[1])):
+            if (position[0]+i >= 0 and position[0]+i < self.graph.shape[0] and 
+                position[1]+j >= 0 and position[1]+j < self.graph.shape[1] and
+                self.graph[position[0]+i][position[1]+j] != 0):
                 neighbors.append((position[0]+i,position[1]+j))
         return neighbors
 
     def make_wall(self,position):
         for pos in position:
             self.graph[pos] = 0
+        print(self.graph)
 
 pos_start = (1,0)
 finish = (0,2)
