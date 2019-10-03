7f739f59b3042d980c87a8cbf78f90415b407eff
skkywalker <lucas@hehl.com.br>
2019-10-03 16:36:36 -0300

    Impose minimum linear speed and what happens then
    Now it is possible to choose the minimum linear speed (min_speed) so
    that the robot won't be stuck with rotation only

--- matlab-testing/diff_robot.m
+++ matlab-testing/diff_robot.m
@@ -3,15 +3,17 @@ close all
 
 omega_log = [0];
 vel_log = [0];
+wl = [0];
+wr = [0];
 
 % Reference to Real World
 real_size = 1.2;
 motor_rpm = 30;
 motor_rad = motor_rpm*2*pi/60;
-linear_speed = 0.5;
+min_speed = 0.5;
 
 % Load map
-map = imread('../src/test.png');
+map = imread('../src/test3.png');
 map_size = size(map);
 %map = imresize(map, [map_size(1) map_size(2)]/2);
 relation_real_img = map_size(2)/real_size; % pixels per meter
@@ -34,8 +36,9 @@ grid = binaryOccupancyMap(walls,relation_real_img);
 mapInflated = copy(grid);
 inflate(mapInflated, 0.7*robot.TrackWidth);
 prm = robotics.PRM(mapInflated);
-prm.NumNodes = 200;
+prm.NumNodes = 300;
 prm.ConnectionDistance = 1;
+show(prm)
 path = findpath(prm, start/relation_real_img, finish/relation_real_img);
 
 robotInitialLocation = path(1,:);
@@ -47,8 +50,8 @@ robotCurrentPose = [robotInitialLocation initialOrientation]';
 
 controller = controllerPurePursuit;
 controller.Waypoints = path;
-controller.DesiredLinearVelocity = linear_speed*motor_rad*robot.WheelRadius;
-controller.MaxAngularVelocity = 2*robot.WheelRadius*motor_rad*(1-linear_speed)/robot.TrackWidth;
+controller.DesiredLinearVelocity = motor_rad*robot.WheelRadius;
+controller.MaxAngularVelocity = 2*robot.WheelRadius*motor_rad*(1-min_speed)/robot.TrackWidth;
 controller.LookaheadDistance = 0.12;
 goalRadius = 0.01;
 distanceToGoal = norm(robotInitialLocation - robotGoal);
@@ -73,16 +76,20 @@ while( distanceToGoal > goalRadius )
     % Get the robot's velocity using controller inputs
     vel = derivative(robot, robotCurrentPose, [v omega]);
     
-%     v = vel(1) + 1i*vel(2);
-%     
-%     if (v > robot.WheelRadius*motor_rad-abs(vel(3))*robot.TrackWidth/2)
-%         new_v = robot.WheelRadius*motor_rad-abs(vel(3))*robot.TrackWidth/2;
-%         vel(1) = new_v*cos(angle(v));
-%         vel(2) = new_v*sin(angle(v));
-%     end
+    v = vel(1) + 1i*vel(2);
+
+    if (abs(v) > motor_rad*robot.WheelRadius-abs(vel(3))*robot.TrackWidth/2)
+     new_v = motor_rad*robot.WheelRadius-abs(vel(3))*robot.TrackWidth/2;
+     vel(1) = new_v*cos(angle(v));
+     vel(2) = new_v*sin(angle(v));
+    end
     
-    vel_log = [vel_log sqrt(vel(1)^2+vel(2)^2)];
-    omega_log = [omega_log vel(3)];
+%     v = sqrt(vel(1)^2+vel(2)^2);
+%     
+%     vel_log = [vel_log sqrt(vel(1)^2+vel(2)^2)];
+%     omega_log = [omega_log vel(3)];
+%     wl = [wl (-vel(3)*robot.TrackWidth/2+v)/robot.WheelRadius];
+%     wr = [wr (vel(3)*robot.TrackWidth/2+v)/robot.WheelRadius];
 
     % Update the current pose
     robotCurrentPose = robotCurrentPose + vel*sampleTime;
@@ -94,6 +101,8 @@ while( distanceToGoal > goalRadius )
     hold off
     show(grid);
     hold all
+    plot(robotInitialLocation(1),robotInitialLocation(2), '.g', 'MarkerSize', 20, 'LineWidth', 2);
+    plot(robotGoal(1), robotGoal(2), '.b', 'MarkerSize', 20, 'LineWidth', 2);
     
     % Plot lookahead point
     plot(lookahead(1),lookahead(2),'b*');
