58055fd14eab8ae33bdc0a338e47b0672c3cbd39
skkywalker <lucas@hehl.com.br>
2019-12-03 20:13:48 -0300

    Improved robot class code

--- libs/robot.py
+++ libs/robot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import time
 
 class DifferentialDrive():
@@ -56,27 +57,42 @@ def lookahead(path,la,i):
         else:
             return pos
 
-def plot_arrow(x, y, yaw, length=0.05, width=0.05, fc="r", ec="k"):
-    if not isinstance(x, float):
-        for (ix, iy, iyaw) in zip(x, y, yaw):
-            plot_arrow(ix, iy, iyaw)
-    else:
-        plt.arrow(x, y, length * math.cos(yaw), length * math.sin(yaw),
-                  fc=fc, ec=ec, head_width=width, head_length=width)
-        plt.plot(x, y)
+def plot_arrow(robot):
+    # Main arrow
+    ax1.arrow(robot.x, robot.y, 0.1 * math.cos(robot.yaw), 0.1 * math.sin(robot.yaw),
+                 fc='r', ec='k', head_width=0.05, head_length=0.05)
+    # Sides
+    ax1.arrow(robot.x, robot.y, robot.L/2 * math.cos(robot.yaw+math.pi/2), robot.L/2 * math.sin(robot.yaw+math.pi/2),
+                 ec='k', head_width=2*robot.R, head_length=0)
+    ax1.arrow(robot.x, robot.y, robot.L/2 * math.cos(robot.yaw-math.pi/2), robot.L/2 * math.sin(robot.yaw-math.pi/2),
+                 ec='k', head_width=2*robot.R, head_length=0.)
 
-robot = DifferentialDrive(0.1,0.5,0.5,0.5,0,1.0,0.2)
 
-i = 0
-while(robot.yaw < 3.1415/2):
-    now = time.time()
-    plt.cla()
-    plot_arrow(robot.x, robot.y, robot.yaw)
-    plt.axis("equal")
-    plt.grid(True)
-    plt.title("Tempo (s):" + str(i))
-    plt.pause(0.0001)
-    passed = time.time()-now
-    i += passed
-    robot.update(passed)
+def update(frame):
+    global last_updated
+    global robot
+    ax1.clear()
+    ax1.imshow(img,extent=[0, 2, 0, 1.5])
+    ax1.set_ylim([0, 1.5])
+    ax1.set_xlim([0, 2])
+    ax1.set_title(str(round(time.time()-init_time,1)) + " segundos")
+    plot_arrow(robot)
+    
+    robot.update(time.time()-last_updated)
+    last_updated = time.time()
+    if(robot.x > 0.9):
+        ani.event_source.stop()
+    
+
+robot = DifferentialDrive(0.1,0.06,0.5,0.5,0,0,3.1415/4)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+img = plt.imread('../map-pics/test2.png') # CONSERTAR ISSO
+init_time = time.time()
+last_updated = init_time
+
+ani = animation.FuncAnimation(fig, update, interval=100)
+plt.show()
+    
 #robot.pure_pursuit((0,0),[(1,1)],1,1)
