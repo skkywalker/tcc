c7d71145840491434fd1e4a7df26c4b43c593c58
skkywalker <lucas@hehl.com.br>
2019-12-19 14:33:26 -0300

    Changes blob detection range

--- libs/get_robot_xy.py
+++ libs/get_robot_xy.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def get_robot_xy(frame):
     
-    lower_red = np.array([50,10,0])
-    upper_red = np.array([255,150,100])
+    lower_red = np.array([200,200,0])
+    upper_red = np.array([255,255,100])
     mask1 = cv2.inRange(frame, lower_red, upper_red)
     return mask1
