d12c634a6e1f45df18a98fd1d72565e53a92ee41
skkywalker <lucas@hehl.com.br>
2019-12-19 15:34:40 -0300

    Change plot_arrow.py to reflect robot.py changes

--- libs/plot_arrow.py
+++ libs/plot_arrow.py
@@ -3,6 +3,6 @@ import matplotlib.animation as animation
 import math
 
 def plot_arrow(robot,axis):
-    axis.arrow(robot.x, robot.y, robot.SIZE * math.cos(robot.yaw), robot.SIZE * math.sin(robot.yaw), fc='r', ec='k', head_width=0.05, head_length=0.05)
-    axis.arrow(robot.x, robot.y, robot.L/2 * math.cos(robot.yaw+math.pi/2), robot.L/2 * math.sin(robot.yaw+math.pi/2), ec='k', head_width=2*robot.R, head_length=0)
-    axis.arrow(robot.x, robot.y, robot.L/2 * math.cos(robot.yaw-math.pi/2), robot.L/2 * math.sin(robot.yaw-math.pi/2), ec='k', head_width=2*robot.R, head_length=0.)
+    axis.arrow(robot.x, robot.y, robot.lenght * math.cos(robot.yaw), robot.lenght * math.sin(robot.yaw), fc='r', ec='k', head_width=0.05, head_length=0.05)
+    axis.arrow(robot.x, robot.y, robot.width/2 * math.cos(robot.yaw+math.pi/2), robot.width/2 * math.sin(robot.yaw+math.pi/2), ec='k', head_width=2*robot.wheel_radius, head_length=0)
+    axis.arrow(robot.x, robot.y, robot.width/2 * math.cos(robot.yaw-math.pi/2), robot.width/2 * math.sin(robot.yaw-math.pi/2), ec='k', head_width=2*robot.wheel_radius, head_length=0.)
