b3236362ff0ef56a024323252290af6e727482e0
skkywalker <lucas@hehl.com.br>
2020-01-15 15:43:51 -0300

    Update new protocol on starwars playing file

--- real_star-wars.py
+++ real_star-wars.py
@@ -4,26 +4,27 @@ import time
 def playSound(dest, tone, beats):
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp.connect(dest)
-    tcp.send(tone.encode())
-    data = tcp.recv(1)
+    print(tone)
+    tcp.send(tone)
+    time.sleep(0.01)
     tcp.close()
     time.sleep(beats/bps)
 
-DO = '065065'
-RE = '075075'
-MI = '083083'
-FA = '090090'
-SOL = '100100'
-DO2 = '130130'
+DO = 2*bytes([65])
+RE = 2*bytes([75])
+MI = 2*bytes([83])
+FA = 2*bytes([90])
+SOL = 2*bytes([100])
+DO2 = 2*bytes([130])
 
 notes = [DO, SOL, FA, MI, RE, DO2, SOL, FA, MI, RE, DO2, SOL, FA, MI, FA, RE]
 beats = [2,2,1/3,1/3,1/3,2,1,1/3,1/3,1/3,2,1,1/3,1/3,1/3,2]
 
 
-dest = ('192.168.1.172', 8888)
+dest = ('192.168.1.181', 8888)
 bps = 108/60
 
 for i,note in enumerate(notes):
     playSound(dest, note, beats[i])
 
-playSound(dest,'000000', 1)
+playSound(dest,2*bytes([0]), 1)
